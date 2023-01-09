cd18e551ec7ab2601641dc544d583ad53eadabcf
mihkelvaher <mihkel.vaher@industry62.com>
2023-01-09 20:49:08 +0200

    Reacti algus

---
+++ veebipood/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img className="pilt" src="https://estonia.ee/wp-content/uploads/nobe_netist_4.jpg" alt="" />
+      <button className="nupp">Ostukorvi</button>
+      
+    </div>
+  );
+}
+
+export default App;
+
+// find error 
+
+// alternative text
+// 1. kui pilti kätte ei saada sealt veebilehelt, siis näidatakse alternative texti
+// 2. pimedatele loetakse ette alt   WCAG    EU nõue, et riigiasutusel peab läbima testi
+// 3. arendajatele info mis seal pildi taga asub
+// ctrl + ä kommentaar
+
+/*
+
+sadas
+
+asd
+
+asdas
+*/
